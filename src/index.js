fb8c3e307f43e647950bc53fc7b52b9c34c9d35f
Volodymyr <allgoodsbazaaar@gmail.com>
2023-10-28 21:15:02 +0300

    update code

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="thermos">
+    <BrowserRouter basename="Thermos">
       <App />
     </BrowserRouter>
   </React.StrictMode>
